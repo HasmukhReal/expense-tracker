cd03590efe8e9881fbea191a5dfe73d5f7bbb1e6
Nihal <patankarnihal@gmail.com>
2026-03-04 16:18:50 +0530

    Commit 2: Implement file reading, update choice menu, add match-case placeholders, store in expenses dict etc.
    - Added functionality to read data from file
    - Stored the read data into the `expenses` dictionary
    - Updated choice options to include viewing mode, as "v"
    - Created match-case placeholders for all choices except "a"
    - Changed expense_record function to expense_add
    - Called expense-add in add mode or "a" case, with argument as date, taken input from user

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,5 +1,16 @@
 expenses = {}
-def expense_record(func_date):
+
+#------------------------------- Data reading from file -------------------------------#
+with open("data.txt", 'r') as file:
+    for line in file:
+        line = line.strip()
+        date, amount, reason = line.split('|')
+        amount = int(amount)
+        expenses[date] = [{"amount": amount, "reason": reason}]
+#--------------------------------------------------------------------------------------#
+
+#------------------------------- Expense Adding Function -------------------------------#
+def expense_add(func_date):
     while True:
         choice2 = input(f"Do you wanna add expense amount for {func_date}?(y/n): ")
         if choice2.lower() == 'y':
@@ -14,13 +25,24 @@ def expense_record(func_date):
             break
         else:
             print("Wrong Input... Try Again!")
+#---------------------------------------------------------------------------------------#
 
 while True:
     print("What do you wanna do?")
-    print("a = Add Expense\nr = Remove Expense\ns = Save\nq = Quit\nsq = Save and Quit")
+    print("a = Add Expense\nr = Remove Expense\nv = View Expense Data\ns = Save\nq = Quit\nsq = Save and Quit")
     choice = input("")
 
     match choice.lower():
         case "a":
             date = input("Enter Date(dd-mm-yyyy): ")
-
+            expense_add(date)
+        case "r":
+            pass
+        case "v":
+            pass
+        case "s":
+            pass
+        case "q":
+            break
+        case "sq":
+            pass
